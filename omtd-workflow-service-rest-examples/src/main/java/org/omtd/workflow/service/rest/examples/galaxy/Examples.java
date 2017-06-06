bda201f6f05a6ae09427f1293a33d711892208d8
ilsp <galanisd@gmail.com>
2017-06-06 03:07:44 +0300

    updated

--- omtd-workflow-service-rest-examples/src/main/java/org/omtd/workflow/service/rest/examples/galaxy/Examples.java
+++ omtd-workflow-service-rest-examples/src/main/java/org/omtd/workflow/service/rest/examples/galaxy/Examples.java
@@ -1,5 +1,14 @@
 package org.omtd.workflow.service.rest.examples.galaxy;
-
+ 	
 public class Examples {
 
+	public static void main(String args[]){
+		String galaxyInstanceUrl = "http://snf-754063.vm.okeanos.grnet.gr";
+		String galaxyApiKey = "e8ea6ff4be6b372344fa6ad123c9d390";
+		
+		Galaxy galaxy = new Galaxy(galaxyInstanceUrl, galaxyApiKey);
+		//galaxy.runWorkflow("/home/ilsp/Desktop/DG/OMTD/omtd-simple-workflows/testInput/", "DGTest1");
+		galaxy.runWorkflow("/home/ilsp/Desktop/smallPDFs/", "DGTest1");
+		
+	}
 }
