ef95315878b28432d02be8fa0a7103da398a8d9f
ilsp <galanisd@gmail.com>
2017-07-13 17:05:13 +0300

    Added build-workflow-service-rest.sh

---
+++ build-workflow-service-rest.sh
@@ -0,0 +1,15 @@
+
+cd ..
+
+# build omtd-workflow-service
+cd omtd-workflow-service
+rm -rf target/*
+mvn clean install -DskipTests=true
+
+read -rsp $'Press enter to continue...\n'
+cd ..
+
+# build 
+cd omtd-workflow-service-rest
+rm -rf target/*
+mvn clean install
