5d868555ccdecf732bd9dec09b400aa3864f87e0
ilsp <galanisd@gmail.com>
2017-07-26 19:24:51 +0300

    Updated

--- scripts/runWorkflowService.sh
+++ scripts/runWorkflowService.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 java -jar ../omtd-workflow-service-rest-server/target/omtd-workflow-service-rest-server-0.0.1-SNAPSHOT.jar --spring.config.location=./application.properties
-pause;
+
 
