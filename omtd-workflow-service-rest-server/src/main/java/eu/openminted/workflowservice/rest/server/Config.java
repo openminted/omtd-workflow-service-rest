bad87107d56c01011753f26dfd5c117037c32215
ilsp <galanisd@gmail.com>
2017-07-26 19:36:44 +0300

    * Removed exclusion * Started using MessageServiceConnector.

--- omtd-workflow-service-rest-server/src/main/java/eu/openminted/workflowservice/rest/server/Config.java
+++ omtd-workflow-service-rest-server/src/main/java/eu/openminted/workflowservice/rest/server/Config.java
@@ -1,18 +1,23 @@
 package eu.openminted.workflowservice.rest.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import eu.openminted.registry.core.jms.service.JMSService;
+import eu.openminted.messageservice.connector.MessageServiceConnector;
 import eu.openminted.workflow.api.WorkflowService;
 import eu.openminted.workflow.service.WorkflowServiceImpl2;
 
 @Configuration
 public class Config {
 
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public WorkflowService getWorkflowService(){	
 		return new WorkflowServiceImpl2();
@@ -29,7 +34,7 @@ public class Config {
     }
 	
 	@Bean
-	public JMSService getJMSService(){
-		return new JMSService();
+	public MessageServiceConnector getJMSService(){
+		return new MessageServiceConnector(env.getProperty("jms.host"));
 	}
 }
