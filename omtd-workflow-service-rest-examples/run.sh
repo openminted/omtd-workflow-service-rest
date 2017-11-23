336b898a4bbd5fc57b6b60b3a8fbcf1f7ef1765d
ilsp <galanisd@gmail.com>
2017-11-23 15:47:40 +0200

    * Updates run.sh

--- omtd-workflow-service-rest-examples/run.sh
+++ omtd-workflow-service-rest-examples/run.sh
@@ -3,5 +3,5 @@
 execJar="/home/ilsp/Desktop/DG/OMTD/omtd-workflow-service-rest/omtd-workflow-service-rest-examples/target/omtd-workflow-service-rest-examples-0.0.1-SNAPSHOT-exec.jar"
 
 # params 
-# storeEndpoint workflowEndpoint inFolder inArchive wid downloadPath
+# storeEndpoint workflowEndpoint inFolder inArchive workflowID downloadPath
 java -jar $execJar $@
