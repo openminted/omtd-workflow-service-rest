040e37b8b15d4423112c621c9af7e9b907b57c19
Stefanos Gatsios <stevengatsios@gmail.com>
2018-02-21 17:30:27 +0200

    Changed Configuration to remove dependencies from omtd-message

--- omtd-workflow-service-rest-server/src/main/java/eu/openminted/workflowservice/rest/server/Config.java
+++ omtd-workflow-service-rest-server/src/main/java/eu/openminted/workflowservice/rest/server/Config.java
@@ -1,25 +1,17 @@
 package eu.openminted.workflowservice.rest.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import eu.openminted.workflow.beans.JmsConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import eu.openminted.messageservice.connector.MessageServicePublisher;
-import eu.openminted.messageservice.connector.MessageServiceSubscriber;
-import eu.openminted.workflow.api.WorkflowService;
-import eu.openminted.workflow.service.WorkflowServiceImpl;
-import eu.openminted.workflow.service.WorkflowServiceMessagesHandler;
-
 @Configuration
+@ComponentScan(basePackageClasses = {JmsConfiguration.class})
 public class Config {
 
-	@Autowired
-	private Environment env;
-		
 	@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
@@ -29,23 +21,4 @@ public class Config {
             }
         };
     }
-	
-	@Bean
-	public WorkflowService getWorkflowService(){	
-		return new WorkflowServiceImpl(getMessageServicePublisher(), getMessageServiceSubscriber());
-	}
-
-	@Bean
-	public MessageServicePublisher getMessageServicePublisher(){
-		return new MessageServicePublisher(env.getProperty("messageService.host"));
-	}
-
-	@Bean
-	public MessageServiceSubscriber getMessageServiceSubscriber(){
-		
-		MessageServiceSubscriber subscriber = new MessageServiceSubscriber(env.getProperty("messageService.host"));
-		subscriber.setMessagesHandler(new WorkflowServiceMessagesHandler());
-		return subscriber;
-	}
-	
 }
